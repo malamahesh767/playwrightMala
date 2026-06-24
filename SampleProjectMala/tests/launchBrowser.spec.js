323819621250d8a48bc05747c506408a402bcfbe
malashree <malamahesh767@gmail.com>
2026-06-24 08:22:14 +0530

    login

--- SampleProjectMala/tests/launchBrowser.spec.js
+++ SampleProjectMala/tests/launchBrowser.spec.js
@@ -2,7 +2,7 @@ const {test, expect}=require('@playwright/test')
 
 test("Launch Browser",async({page})=>
 {
-  await page.goto("https://sgtestinginstituteapp.onrender.com/login");
+  await page.goto("https://sgtestinginstituteapp.onrender.com/");
   await page.waitForTimeout(3000);
   console.log("Browser Launched Successfully");
 })
